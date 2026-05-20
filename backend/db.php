6701cc3109ee0e344f9fb57b12f8da566bd94813
Emeline <jonneauxemeline3@gmail.com>
2026-05-20 11:51:27 +0200

    ajout config heroku

--- backend/db.php
+++ backend/db.php
@@ -1,19 +1,37 @@
 <?php
 
-$host = "localhost";
-$dbname = "ecf";
-$username = "root";
-$password = "";
+if (getenv("JAWSDB_URL")) {
+    $url = parse_url(getenv("JAWSDB_URL"));
+
+    $host = $url["host"];
+    $dbname = substr($url["path"], 1);
+    $username = $url["user"];
+    $password = $url["pass"];
+    $port = $url["port"] ?? 3306;
+
+} else {
+    $host = "localhost";
+    $dbname = "ecf";
+    $username = "root";
+    $password = "";
+    $port = 3306;
+}
 
 try {
+
     $pdo = new PDO(
-        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8",
         $username,
         $password
     );
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
 
 } catch (PDOException $e) {
+
     die("Erreur connexion DB : " . $e->getMessage());
+
 }
